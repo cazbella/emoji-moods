bda92ad2ec23439448689442579ae429ff319bd5
CarolineBellaLane <carolinelane@live.co.uk>
2024-02-27 20:49:40 +0000

    adds file structure and assignment requirements as comments

---
+++ assets/logic/logic.js
@@ -0,0 +1,27 @@
+// Question 1
+// (32 marks)
+// Create a web app that takes input from the user and updates the web page based on the 
+// input. The HTML and CSS should be simple but present. The primary focus is on JavaScript. 
+// Remember to come up with a unique creative problem or scenario!
+// You will be marked on:
+// A. Knowledge and demonstrated use of Javascript (13 marks)
+// B. Knowledge and demonstrated use of HTML and CSS (10 marks)
+// C. Code readability, layout, and use of best practices (4 marks)
+// D. Creativity (5 marks)
+// You should:
+// + Use boolean values and if..else statements to branch logic of your program 
+// + Use a data structure like an array to store values
+// + Use a loop or a while loop to reduce repetition
+// + Use functions to make code reusable
+// + Use console.log and alert to display messages
+// + Use some HTML and CSS to create a basic website. Use at least 4 different HTML tags 
+// and style at least 2 of them. This does not need to look good! It is just a demo of what is 
+// covered.
+// + Get input from the user on a web page
+// + Make changes to the HTML or CSS  using JavaScript
+// + Use an event to trigger a change to a web page
+// If you cannot come up with a creative idea you can use this scenario, but 5 points out of the 
+// total mark are for creativity and you will receive 0 for creativity:
+// "A website that allows users to customise their lunch order by selecting from several options like 
+// in a Subway. After selection, the website offers to make it a meal deal with a drink and a snack. At
+// the end, it displays a new total depending on prices. The website has a simple style with a couple 
