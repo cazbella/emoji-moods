63a15b6d4493ac2f49f21bbfe4df7265d5d5e13a
CarolineBellaLane <carolinelane@live.co.uk>
2024-02-29 14:19:46 +0000

    adds more html elements

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -1,5 +1,4 @@
-// Question 1
-// (32 marks)
+
 // Create a web app that takes input from the user and updates the web page based on the 
 // input. The HTML and CSS should be simple but present. The primary focus is on JavaScript. 
 // Remember to come up with a unique creative problem or scenario!
