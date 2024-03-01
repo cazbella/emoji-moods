b29b00f3218e34e65274906ab0d74ad1389274c0
CarolineBellaLane <carolinelane@live.co.uk>
2024-03-01 15:49:58 +0000

    adds inspirational quote array

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -58,7 +58,111 @@ let sadEmojiArray = [
 //match user choice to array content/position
 //need event listeners for see my mood button
 
-//
+//loop through quote array instead
+//https://www.southernliving.com/culture/sad-quotes
+
+const inspirationalQuotes = [
+    {
+        quote: "Believe you can and you're halfway there.",
+        author: "Theodore Roosevelt"
+    },
+    {
+        quote: "Keep your face always toward the sunshine - and shadows will fall behind you.",
+        author: "Walt Whitman"
+    },
+    {
+        quote: "In the middle of difficulty lies opportunity.",
+        author: "Albert Einstein"
+    },
+    {
+        quote: "You are never too old to set another goal or to dream a new dream.",
+        author: "C.S. Lewis"
+    },
+    {
+        quote: "The only limit to our realization of tomorrow will be our doubts of today.",
+        author: "Franklin D. Roosevelt"
+    },
+    {
+        quote: "The best way to predict the future is to create it.",
+        author: "Abraham Lincoln"
+    },
+    {
+        quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        author: "Winston Churchill"
+    },
+    {
+        quote: "Life is 10% what happens to us and 90% how we react to it.",
+        author: "Charles R. Swindoll"
+    },
+    {
+        quote: "It always seems impossible until it's done.",
+        author: "Nelson Mandela"
+    },
+    {
+        quote: "Your time is limited, don't waste it living someone else's life.",
+        author: "Steve Jobs"
+    },
+    {
+        quote: "Success is not the key to happiness. Happiness is the key to success.",
+        author: "Albert Schweitzer"
+    },
+    {
+        quote: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt"
+    },
+    {
+        quote: "You miss 100% of the shots you don't take.",
+        author: "Wayne Gretzky"
+    },
+    {
+        quote: "Happiness is not by chance, but by choice.",
+        author: "Jim Rohn"
+    },
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela"
+    },
+    {
+        quote: "Change your thoughts and you change your world.",
+        author: "Norman Vincent Peale"
+    },
+    {
+        quote: "Don't cry because it's over, smile because it happened.",
+        author: "Dr. Seuss"
+    },
+    {
+        quote: "You must be the change you wish to see in the world.",
+        author: "Mahatma Gandhi"
+    },
+    {
+        quote: "Happiness is not something ready made. It comes from your own actions.",
+        author: "Dalai Lama"
+    },
+    {
+        quote: "The only way to achieve the impossible is to believe it is possible.",
+        author: "Charles Kingsleigh"
+    },
+    {
+        quote: "Opportunities don't happen, you create them.",
+        author: "Chris Grosser"
+    },
+    {
+        quote: "The purpose of our lives is to be happy.",
+        author: "Dalai Lama"
+    },
+    {
+        quote: "Success is walking from failure to failure with no loss of enthusiasm.",
+        author: "Winston Churchill"
+    },
+    {
+        quote: "The only way to do great work is to love what you do.",
+        author: "Steve Jobs"
+    },
+];
+
+console.log(inspirationalQuotes);
+
+
 
 
 
