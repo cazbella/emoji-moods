ee95c9611061d8fab65c069bc71523c27f98fb5b
CarolineBellaLane <carolinelane@live.co.uk>
2024-03-01 12:24:57 +0000

    pseudocodes next parts of javascript

--- assets/logic/logic.js
+++ assets/logic/logic.js
@@ -3,9 +3,9 @@ const startButton = document.querySelector('.start-button');
 
 //https://www.shecodes.io/athena/11102-how-to-trigger-a-prompt-on-button-click-in-javascript#:~:text=addEventListener('click'%2C%20function,log(userInput)%3B%20%7D)%3B
 // Add an event listener to the button that listens for the click 'event'
-startButton.addEventListener('click', function() {
+startButton.addEventListener('click', function () {
     // Display the prompt when the button is clicked
-    
+
     const userInput = prompt('Are you happy or sad?').toLowerCase(); //to make input lowercase
     //hide after clicked
     startButton.style.display = 'none';
@@ -14,7 +14,28 @@ startButton.addEventListener('click', function() {
     const happyDiv = document.querySelector('.happy');
     const sadDiv = document.querySelector('.sad');
 
-}
+// Clear the content of the emoji and quote containers if user wants another go
+//changed to move button so need to figure this out
+// document.getElementById('emojiContainer').innerHTML = '';
+// document.getElementById('quoteContainer').innerHTML = '';
+
+// need to the correct dropdown based on user input
+//boolean
+//if statement
+
+//need array of emojis
+
+//need to use happy or sad array depending on choice
+
+//need a for loop
+//match user choice to array content/position
+//need event listeners for see my mood button
+
+//
+
+
+
+
 
 // Create a web app that takes input from the user and updates the web page based on the 
 // input. The HTML and CSS should be simple but present. The primary focus is on JavaScript. 
@@ -36,8 +57,3 @@ startButton.addEventListener('click', function() {
 // + Get input from the user on a web page YES
 // + Make changes to the HTML or CSS  using JavaScript
 // + Use an event to trigger a change to a web page
-// If you cannot come up with a creative idea you can use this scenario, but 5 points out of the 
-// total mark are for creativity and you will receive 0 for creativity:
-// "A website that allows users to customise their lunch order by selecting from several options like 
-// in a Subway. After selection, the website offers to make it a meal deal with a drink and a snack. At
-// the end, it displays a new total depending on prices. The website has a simple style with a couple 
